a0a0b7f3cff8aec2e7f136deec902a861a98aa19
GMH501 <gabrielmeghnagi@outlook.it>
2021-01-06 19:51:32 +0100

    Stable

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, jsonify
 
-app = Flask(__file__)
+app = Flask(__name__)
 
 
-@route("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
-app.run()
+app.run(host="0.0.0.0", port=8080)
